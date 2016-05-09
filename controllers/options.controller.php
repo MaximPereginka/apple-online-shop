d36e1e2d1bd375b53f01c01b62808369376864cf
Maxim Pereginka <Maxim Pereginka>
2016-05-09 15:02:12 +0300

    Options - FIX: Page finished

--- controllers/options.controller.php
+++ controllers/options.controller.php
@@ -20,6 +20,23 @@ Class OptionsController extends Controller
     //Методы админки
     //Выводит настройки сайта
     public function administrator_index(){
-
+        $this->data['options'] = $this->model->get_options();
+        
+        if($_POST){
+            //Сохраняем настройки
+            if(!$this->model->save(6, $_POST['site_name'])) {
+                Session::setMessage("Не удалось сохранить название сайта");
+            }
+            else if(!$this->model->save(7, $_POST['site_description'])) {
+                Session::setMessage("Не удалось сохранить описание сайта");
+            }
+            else if(!$this->model->save(8, $_POST['site_keywords'])) {
+                Session::setMessage("Не удалось сохранить ключевые слова сайта");
+            }
+            else {
+                Session::setMessage("Настройки успешно сохранены");
+                Router::redirect("/administrator/options");
+            }
+        }
     }
 }
--- models/options.php
+++ models/options.php
@@ -9,5 +9,26 @@
 //Отвечает за передачу и обработку данных на страницах с настройками сайта
 Class Options extends Model
 {
-    
+    public function get_options(){
+        $sql = "
+            SELECT *
+            FROM options
+        ";
+        
+        return $this->db->query($sql);
+    }
+
+    //Сохраняет настройку по её названию в базе и значению
+    public function save($id, $value){
+        $value = $this->db->escape($value);
+        $id = (int)$id;
+
+        $sql = "
+            UPDATE options
+            SET value = '".$value."'
+            WHERE option_id = '".$id."'
+        ";
+
+        return $this->db->query($sql);
+    }
 }
